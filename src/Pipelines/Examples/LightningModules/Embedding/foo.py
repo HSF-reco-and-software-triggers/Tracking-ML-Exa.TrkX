7b227201febbb01b84f4cb84e22a4b23339564ef
Daniel <murnanedaniel@hotmail.com>
2020-11-09 14:19:53 -0800

    Update docs

--- src/Pipelines/Examples/LightningModules/Embedding/foo.py
+++ src/Pipelines/Examples/LightningModules/Embedding/foo.py
@@ -1,3 +1,35 @@
+# System imports
+import sys
+import os
+
+# 3rd party imports
+import pytorch_lightning as pl
+from pytorch_lightning import LightningModule
+import torch
+from torch.nn import Linear
+from torch.utils.data import random_split
+from torch_geometric.data import DataLoader
+from torch_cluster import radius_graph
+import numpy as np
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Local imports
+from .utils import graph_intersection, build_edges, res
+
+def load_datasets(input_dir, train_split, seed = 0):
+    '''
+    Prepare the random Train, Val, Test split, using a seed for reproducibility. Seed should be
+    changed across final varied runs, but can be left as default for experimentation.
+    '''
+    torch.manual_seed(seed)
+    all_events = os.listdir(input_dir)
+    all_events = sorted([os.path.join(input_dir, event) for event in all_events])
+    loaded_events = [torch.load(event) for event in all_events[:sum(train_split)]]
+    train_events, val_events, test_events = random_split(loaded_events, train_split)
+
+    return train_events, val_events, test_events
+
 class Bar:
     """
     A test class
