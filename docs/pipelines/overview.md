6fb15933845b45e806dc898bfd8cf8a811fd3698
Daniel <murnanedaniel@hotmail.com>
2020-11-10 10:34:29 -0800

    Update docs

--- docs/pipelines/overview.md
+++ docs/pipelines/overview.md
@@ -9,24 +9,31 @@
 
 Follow the tutorial in [Quickstart](https://hsf-reco-and-software-triggers.github.io/Tracking-ML-Exa.TrkX/pipelines/quickstart) to get up and running quickly with a toy model.
 
+
+## How do Pipelines work?
+
+The aim of our pipeline structure is to abstract out as much repetitive code as possible. A pipeline is defined by a YAML config file, which only requires three inputs: The location of your model definitions `model_library`, the location to save/load your artifacts `artifact_library`, and the stages of the pipeline `stage_list`. An example stage list is
+```yaml
+- {set: GNN, name: SliceCheckpointedResAGNN, config: train_gnn.yaml}
+```
+The `set` key defines the type of ML model (to help avoid naming ambiguity), the `name` key is the class of the model, and the `config` key is the file specifying your choice of hyperparameters, directories, callbacks and so on. And that's it!
+
 <figure>
-  <img src="https://raw.githubusercontent.com/HSF-reco-and-software-triggers/Tracking-ML-Exa.TrkX/master/docs/media/application_diagram_1.png"/>
+  <img src="https://raw.githubusercontent.com/HSF-reco-and-software-triggers/Tracking-ML-Exa.TrkX/master/docs/media/pipeline_diagram_1.png"/>
   <figcaption>The repository through an application lens, using Pipelines for specific physics goals</figcaption>
 </figure>
 
+## Why this choice of abstraction?
+
+I found that my two forms of R&D fell into breadth and depth. Much of the time, I would play at length with hyperparameters and model definitions, in which case I want that all to live in one place: The model's config file. Thus the pipeline config can remain untouched if we have one for each choice of (model, config) , or only changed occasionally if we choose to have only one. At other times, development would require a series of models, where successive results depend on hyperparameter choices earlier in the chain. Then I can play with the higher level pipeline config and try difference (model, config) stages, while the whole chain of hyperparameters is committed to each step via a logging platform (Weights & Biases in my case).
+
+
 
 ## Pytorch Lightning & MLFlow
+
+This repository uses Pytorch Lightning, which allows us to encapsulate all training and model logic into a module object. This module is what is being specified by the pipeline config, with `name`. Combined with callbacks in the model config file, all pipeline logic is contained in each module. A callback object integrates with the module and knows about telemetry and post-processing steps. Rather than a monolithic function that passes data through the pipeline (whereby mistakes could be made), the pipeline asks each model how it should be treated, and then acts with the model's own methods.
 
 
 ## Data Processing & Modularity
 
 Define clearly the data structure of each modular section
-
-
-## Graph Construction
-
-
-## Graph Neutral Network
-
-
-## Post-processing
--- src/Pipelines/Examples/utils/stage_utils.py
+++ src/Pipelines/Examples/utils/stage_utils.py
@@ -12,7 +12,7 @@ def find_config(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
-        
+
 def find_checkpoint(run_id, path):
     for (root_dir, dirs, files) in os.walk(path):
         if run_id in dirs:
@@ -23,48 +23,48 @@ def load_config(stage, resume_id, pipeline_config):
     if resume_id is None:
         with open(find_config(stage["config"], os.path.join(pipeline_config["model_library"], stage["set"]))) as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-        
+
     else:
         ckpnt_path = find_checkpoint(resume_id, pipeline_config["artifact_library"])
         ckpnt = torch.load(ckpnt_path, map_location=torch.device('cpu'))
         config = ckpnt["hyper_parameters"]
-    
+
     if "override" in stage.keys():
         config.update(stage["override"])
-    
+
     config["stage"] = stage
-    
-    return config    
-        
+
+    return config
+
 def find_model(model_set, model_name, model_library):
-        
+
     # List all modules in the set/Models directory
-    module_list = [os.path.splitext(name)[0] for 
+    module_list = [os.path.splitext(name)[0] for
                    name in os.listdir(os.path.join(model_library, model_set, "Models")) if name.endswith(".py")]
-    
+
     # Import all modules in the set/Models directory and find model_name
     imported_module_list = [importlib.import_module('.'.join([model_set, "Models", module])) for module in module_list]
     names = [mod for mod in imported_module_list if model_name in getattr(mod, '__all__', [n for n in dir(mod) if not n.startswith('_')])]
-    
+
     # Return the class of model_name
     model_class = getattr(names[0], model_name)
     return model_class
 
 def build_model(stage, pipeline_config):
-    
+
     model_set = stage["set"]
     model_name = stage["name"]
     model_library = pipeline_config["model_library"]
     config_file = stage["config"]
-    
+
     model_class = find_model(model_set, model_name, model_library)
-    
+
     return model_class
 
 def get_logger(model_config, run_id):
-    
+
     wandb_logger = WandbLogger(project=model_config["project"], save_dir = model_config["wandb_save_dir"], id=run_id)
-   
+
     return wandb_logger
 
 
@@ -73,31 +73,31 @@ def callback_objects(model_config):
     callback_list = model_config["callbacks"]
     model_set = model_config["stage"]["set"]
     callback_object_list = [find_model(model_set, callback)() for callback in callback_list]
-    
+
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
     callback_object_list = callback_object_list + [lr_monitor]
-    
-    return 
+
+    return
 
 def build_trainer(model_config, logger, resume_id, pipeline_config):
-    
+
     model_filepath = os.path.join(pipeline_config["artifact_library"], model_config["project"], logger.experiment._run_id, "last.ckpt")
-    
+
     checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
         filepath=model_filepath,
         save_top_k=1,
         save_last=True,
         mode='min')
-        
+
     # Handle resume condition
     if resume_id is None:
         # The simple case: We start fresh
-        trainer = pl.Trainer(max_epochs = model_config['max_epochs'], gpus=1, logger=logger, checkpoint_callback=checkpoint_callback, callbacks=callback_objects(model_config))    
+        trainer = pl.Trainer(max_epochs = model_config['max_epochs'], gpus=1, logger=logger, checkpoint_callback=checkpoint_callback, callbacks=callback_objects(model_config))
     else:
         # Here we assume
-        trainer = pl.Trainer(resume_from_checkpoint=model_filepath, max_epochs = model_config['max_epochs'], gpus=1, logger=logger, checkpoint_callback=checkpoint_callback, callbacks=callback_objects(model_config))    
-       
+        trainer = pl.Trainer(resume_from_checkpoint=model_filepath, max_epochs = model_config['max_epochs'], gpus=1, logger=logger, checkpoint_callback=checkpoint_callback, callbacks=callback_objects(model_config))
+
     return trainer
 
 
