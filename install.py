cb45a672ff09737f72e2059e205e3e2e8406c55d
Daniel Murnane <murnanedaniel@hotmail.com>
2021-03-10 13:14:13 -0800

    Add install file

--- install.py
+++ install.py
@@ -34,15 +34,15 @@ def main():
     os.environ["CUDA"] = hardware
     # Install requirements
 #     output = install("-r requirements.txt", file_link = "https://download.pytorch.org/whl/torch_stable.html https://pytorch-geometric.com/whl/torch-1.8.0+{}.html".format(hardware))
-    package_1 = "torch_requirements.txt"
+    package_1 = "requirements.txt"
     package_2 = "geometric_requirements.txt"
     file_link_1 = "https://download.pytorch.org/whl/torch_stable.html"
     file_link_2 = "https://pytorch-geometric.com/whl/torch-1.8.0+{}.html".format(hardware)
-    output = subprocess.run([sys.executable, "-m", "pip", "install", "-r", package_1, "-f", file_link_1], capture_output=True)
+    output = subprocess.run([sys.executable, "-m", "pip", "install", "-r", package_1], capture_output=True)
     print(output)
     
-    output = subprocess.run([sys.executable, "-m", "pip", "install", "-r", package_2, "-f", file_link_2], capture_output=True)
-    print(output)
+#     output = subprocess.run([sys.executable, "-m", "pip", "install", "-r", package_2, "-f", file_link_2], capture_output=True)
+#     print(output)
     
     # Install faiss
     # Install pytorch3d
