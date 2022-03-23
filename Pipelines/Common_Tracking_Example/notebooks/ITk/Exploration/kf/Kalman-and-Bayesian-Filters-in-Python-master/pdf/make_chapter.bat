1295a75654917d224d4a13d7cc6e8e7d97dac419
Daniel Murnane <murnanedaniel@hotmail.com>
2022-03-23 09:15:37 -0700

    Remove KalmanFilter

--- Pipelines/Common_Tracking_Example/notebooks/ITk/Exploration/kf/Kalman-and-Bayesian-Filters-in-Python-master/pdf/make_chapter.bat
+++
@@ -1,5 +0,0 @@
-cp %1 chapter.ipynb
-ipython nbconvert --to latex chapter.ipynb
-ipython to_pdf.py chapter.tex
-
-
--- Pipelines/Common_Tracking_Example/notebooks/ITk/Exploration/kf/Kalman-and-Bayesian-Filters-in-Python-master/pdf/run_notebooks.bat
+++
@@ -1,9 +0,0 @@
-mkdir tmp
-copy ..\*.ipynb .\tmp
-copy ..\*.py .\tmp
-cp -r ..\kf_book\ .\tmp\
-
-cd tmp
-
-forfiles /m *.ipynb /c "cmd /c ipython ..\rm_notebook.py @file"
-jupyter nbconvert --allow-errors --inplace --execute *.ipynb
